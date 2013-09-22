c9a345db0b88756cd08f2a39849fd993cadee46c
Mike Cowgill <moofish32@gmail.com>
2013-09-21 21:36:11 -0700

    name issues on deb package

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,6 +43,7 @@ elsif apt_packaging
 
   dpkg_package "typesafe-stack" do
     source "http://apt.typesafe.com/repo-deb-build-0002.deb"
+    package_name "repo-deb-build-0002"
     action :install
   end
 end
